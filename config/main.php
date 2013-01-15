9c7227c442fe6ba3429e67027dcbf452a5848f4b
kengos <kengo@kengos.jp>
2013-01-15 23:22:59 +0900

    fix

--- config/main.php
+++ config/main.php
@@ -1,90 +1,89 @@
 <?php
 
-// uncomment the following to define a path alias
-// Yii::setPathOfAlias('local','path/to/local-folder');
+// composer autoload
+require(dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
 
-// This is the main Web application configuration. Any writable
-// CWebApplication properties can be configured here.
-return array_merge(
-  array(
-    'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'app',
-    'name'=>'My Web Application',
-    'runtimePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'runtime',
-    // preloading 'log' component
-    'preload'=>array('log'),
+$base = array(
+  'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'app',
+  'name'=>'My Web Application',
+  'runtimePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'runtime',
+  // preloading 'log' component
+  'preload'=>array('log'),
 
-    // autoloading model and component classes
-    'import'=>array(
-      'application.models.*',
-      'application.components.*',
+  // autoloading model and component classes
+  'import'=>array(
+    'application.models.*',
+    'application.components.*',
+  ),
+
+  'modules'=>array(
+    // uncomment the following to enable the Gii tool
+    /*
+    'gii'=>array(
+      'class'=>'system.gii.GiiModule',
+      'password'=>'Enter Your Password Here',
+      // If removed, Gii defaults to localhost only. Edit carefully to taste.
+      'ipFilters'=>array('127.0.0.1','::1'),
     ),
+    */
+  ),
 
-    'modules'=>array(
-      // uncomment the following to enable the Gii tool
-      /*
-      'gii'=>array(
-        'class'=>'system.gii.GiiModule',
-        'password'=>'Enter Your Password Here',
-        // If removed, Gii defaults to localhost only. Edit carefully to taste.
-        'ipFilters'=>array('127.0.0.1','::1'),
-      ),
-      */
+  // application components
+  'components'=>array(
+    'user'=>array(
+      // enable cookie-based authentication
+      'allowAutoLogin'=>true,
     ),
-
-    // application components
-    'components'=>array(
-      'user'=>array(
-        // enable cookie-based authentication
-        'allowAutoLogin'=>true,
+    // uncomment the following to enable URLs in path-format
+    /*
+    'urlManager'=>array(
+      'urlFormat'=>'path',
+      'rules'=>array(
+        '<controller:\w+>/<id:\d+>'=>'<controller>/view',
+        '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+        '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
       ),
-      // uncomment the following to enable URLs in path-format
-      /*
-      'urlManager'=>array(
-        'urlFormat'=>'path',
-        'rules'=>array(
-          '<controller:\w+>/<id:\d+>'=>'<controller>/view',
-          '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-          '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+    ),
+    */
+    // uncomment the following to use a MySQL database
+    /*
+    'db'=>array(
+      'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+      'emulatePrepare' => true,
+      'username' => 'root',
+      'password' => '',
+      'charset' => 'utf8',
+    ),
+    */
+    'errorHandler'=>array(
+      // use 'site/error' action to display errors
+      'errorAction'=>'site/error',
+    ),
+    'log'=>array(
+      'class'=>'CLogRouter',
+      'routes'=>array(
+        array(
+          'class'=>'CFileLogRoute',
+          'levels'=>'error, warning',
         ),
-      ),
-      */
-      // uncomment the following to use a MySQL database
-      /*
-      'db'=>array(
-        'connectionString' => 'mysql:host=localhost;dbname=testdrive',
-        'emulatePrepare' => true,
-        'username' => 'root',
-        'password' => '',
-        'charset' => 'utf8',
-      ),
-      */
-      'errorHandler'=>array(
-        // use 'site/error' action to display errors
-        'errorAction'=>'site/error',
-      ),
-      'log'=>array(
-        'class'=>'CLogRouter',
-        'routes'=>array(
-          array(
-            'class'=>'CFileLogRoute',
-            'levels'=>'error, warning',
-          ),
-          // uncomment the following to show log messages on web pages
-          /*
-          array(
-            'class'=>'CWebLogRoute',
-          ),
-          */
+        // uncomment the following to show log messages on web pages
+        /*
+        array(
+          'class'=>'CWebLogRoute',
         ),
+        */
       ),
-    ),
-
-    // application-level parameters that can be accessed
-    // using Yii::app()->params['paramName']
-    'params'=>array(
-      // this is used in contact page
-      'adminEmail'=>'webmaster@example.com',
     ),
   ),
-  require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'db.php'),
+
+  // application-level parameters that can be accessed
+  // using Yii::app()->params['paramName']
+  'params'=>array(
+    // this is used in contact page
+    'adminEmail'=>'webmaster@example.com',
+  ),
+);
+return array_merge(
+  $base,
+  require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'db.php')
 );
